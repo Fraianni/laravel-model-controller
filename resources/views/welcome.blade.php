bd7138ca031370f37be73cc5f764c83abf9f7bf5
Fraianni <francescoianni1995@gmail.com>
2022-11-02 17:00:21 +0100

    Visualizzazione di altre informazioni films

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,11 @@
     <div class="cards-container">
         @foreach ($movies as $movie)
             <div class="card">
-                <h1>{{ $movie['title'] }}</h1>
+                <h4>{{ $movie['title'] }}</h1>
+                    <p>original title:{{ $movie['original_title'] }}</p>
+                    <p> nationality:{{ $movie['nationality'] }}</p>
+                    <p>vote:{{ $movie['vote'] }}</p>
+
 
             </div>
         @endforeach
