12d7059be5fb98eae04fbc68a062cc2a65d5701f
Fraianni <francescoianni1995@gmail.com>
2022-11-02 16:42:58 +0100

    Acccesso ai dati e visualizzazione titoli film

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/app.css">
+
+    <title>Controller</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+<body>
+    <div class="cards-container">
+        @foreach ($movies as $movie)
+            <div class="card">
+                <h1>{{ $movie['title'] }}</h1>
+
+            </div>
+        @endforeach
+    </div>
+
+</body>
+
+</html>
